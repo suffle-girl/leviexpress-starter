90ef63800a9cbb8fd3fc6511d94e6936ee00ccce
Šárka Flídrová <sarka.flidrova@email.cz>
2024-05-18 15:04:59 +0200

    Přidáno automatické sedadlo.

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { JourneyPicker } from '../../components/JourneyPicker';
 import JourneyDetail from '../../components/JourneyDetail';
+import { SelectedSeat } from '../../components/SelectedSeat';
 
 export const HomePage = () => {
   const [journey, setJourney] = useState(null);
@@ -12,6 +13,7 @@ export const HomePage = () => {
     <main>
       <JourneyPicker onJourneyChange={handleJourneyChange} />
       {journey && <JourneyDetail journey={journey.stops} />}
+      {journey && <SelectedSeat number={journey.autoSeat}/> }
     </main>
   );
 };
