9b9b7720d03d370e6b54b1988a39e6a8ff5e185f
Šárka Flídrová <sarka.flidrova@email.cz>
2024-05-18 16:34:26 +0200

    Detail rezervace

--- src/components/ReservationPage/index.jsx
+++ src/components/ReservationPage/index.jsx
@@ -1,5 +1,46 @@
+import { useParams } from "react-router-dom";
+import "./style.css";
+import { useEffect, useState } from "react";
+
 export const ReservationPage = () => {
-    return (
-        <h1>Vaše jízdenka</h1>
-    )
-}
+  const [reservation, setReservation] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://apps.kodim.cz/daweb/leviexpress/api/reservation?id=${id}`
+      );
+      const data = await response.json();
+      const data2 = data.results;
+      setReservation(data2);
+      console.log(data2);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {reservation && (
+        <div className="reservation container">
+          <h2>Vaše e-jízdenka č. </h2>
+          <div className="reservation__body">
+            <div className="reservation__headings">
+              <p>Datum cesty:</p>
+              <p>Odjezd:</p>
+              <p>Příjezd:</p>
+              <p>Sedadlo:</p>
+            </div>
+            <div className="reservation__info">
+                {console.log(reservation)}
+              <p>{reservation.date}</p>
+              <p>{reservation.fromCity}</p>
+              <p>{reservation.toCity}</p>
+              <p>{reservation.seatNumber}</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
