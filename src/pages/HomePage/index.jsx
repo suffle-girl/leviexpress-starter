1a8530c3d3ace7d060848fe636a7581ceb04a812
Denisa Zemanova <xdenisa.zemanova@gmail.com>
2024-05-18 15:34:58 +0200

    Přidána rezervace napojená na API

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -9,11 +9,38 @@ export const HomePage = () => {
     setJourney(journeyData);
   };
 
+  const handleBuy = async () => {
+    console.log('Klik rezervace');
+    const response = await fetch(
+      'https://apps.kodim.cz/daweb/leviexpress/api/reservation',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          action: 'create',
+          seat: journey.autoSeat,
+          journeyId: journey.journeyId,
+        }),
+      },
+    );
+    const data = await response.json();
+    const data2 =data.results.reservationId
+    console.log(data2);
+  };
+
   return (
     <main>
       <JourneyPicker onJourneyChange={handleJourneyChange} />
       {journey && <JourneyDetail journey={journey.stops} />}
-      {journey && <SelectedSeat number={journey.autoSeat}/> }
+      {journey && <SelectedSeat number={journey.autoSeat} />}
+
+      <div className="controls container">
+        <button onClick={handleBuy} className="btn btn--big" type="button">
+          Rezervovat
+        </button>
+      </div>
     </main>
   );
 };
